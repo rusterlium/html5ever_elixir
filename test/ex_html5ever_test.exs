644ca301837fedf9f9dfab4a1b17f2c18f576099
HansiHE <hansihe@hansihe.com>
2017-01-14 00:09:59 +0100

    remove dailymail html

--- test/ex_html5ever_test.exs
+++ test/ex_html5ever_test.exs
@@ -2,8 +2,9 @@ defmodule ExHtml5everTest do
   use ExUnit.Case
   doctest ExHtml5ever
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def read_html(name) do
+    dir = to_string(:code.priv_dir(:ex_html5ever)) <> "/test_data/"
+    File.read!(dir <> name)
   end
 
   test "parse basic html" do
@@ -12,9 +13,13 @@ defmodule ExHtml5everTest do
     assert ExHtml5ever.parse(html) == ret
   end
 
-  test "parse dailymail html" do
-    test_data_dir = to_string(:code.priv_dir(:ex_html5ever)) <> "/test_data/"
-    html = File.read!(test_data_dir <> "dailymail.html")
+  test "parse example.com html" do
+    html = read_html("drudgereport.html")
+    assert match?({:ok, _}, ExHtml5ever.parse(html))
+  end
+
+  test "parse drudgereport.com html" do
+    html = read_html("drudgereport.html")
     assert match?({:ok, _}, ExHtml5ever.parse(html))
   end
 
