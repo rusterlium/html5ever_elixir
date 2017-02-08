3e641a246927b3447ef742d772f6d4b94596790b
Philip Sampaio Silva <philip.sampaio@gmail.com>
2017-02-08 00:26:26 -0200

    Improve README and add licenses

--- lib/html5ever/native.ex
+++ lib/html5ever/native.ex
@@ -5,11 +5,10 @@ end
 defmodule Html5ever.Native do
   use Rustler, otp_app: :html5ever, crate: "html5ever_nif"
 
-  def parse_async(binary), do: err()
-  def parse_sync(binary), do: err()
+  def parse_async(_binary), do: err()
+  def parse_sync(_binary), do: err()
 
   defp err() do
     throw NifNotLoadedError
   end
-
 end
