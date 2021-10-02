1d15a818dc1b5404fa538bbeb90c39a8bfa01879
Philip Sampaio <philip.sampaio@gmail.com>
2021-10-02 02:18:55 -0300

    Format files and add basic docs (#20)
    This also updates ex_doc and related dependencies.

--- lib/html5ever.ex
+++ lib/html5ever.ex
@@ -1,18 +1,86 @@
 defmodule Html5ever do
   @moduledoc """
-  Documentation for ExHtml5ever.
+  The html5ever is an HTML parser written in Rust.
   """
 
+  @doc """
+  Parses an HTML document from a string.
+
+  This returns a list of tuples representing the HTML tree.
+
+  ## Example
+
+      iex> Html5ever.parse("<!doctype html><html><body><h1>Hello world</h1></body></html>")
+      {:ok,
+       [
+         {:doctype, "html", "", ""},
+         {"html", [], [{"head", [], []}, {"body", [], [{"h1", [], ["Hello world"]}]}]}
+       ]}
+
+  """
   def parse(html) when byte_size(html) > 500 do
     parse_async(html)
   end
+
   def parse(html) do
     parse_sync(html)
   end
 
+  @doc """
+  Parses an HTML document from a string and returns a map.
+
+  The map contains the document structure.
+
+  ## Example
+
+      iex> Html5ever.flat_parse("<!doctype html><html><body><h1>Hello world</h1></body></html>")
+      {:ok,
+       %{
+         nodes: %{
+           0 => %{id: 0, parent: nil, type: :document},
+           1 => %{id: 1, parent: 0, type: :doctype},
+           2 => %{
+             attrs: [],
+             children: [3, 4],
+             id: 2,
+             name: "html",
+             parent: 0,
+             type: :element
+           },
+           3 => %{
+             attrs: [],
+             children: [],
+             id: 3,
+             name: "head",
+             parent: 2,
+             type: :element
+           },
+           4 => %{
+             attrs: [],
+             children: [5],
+             id: 4,
+             name: "body",
+             parent: 2,
+             type: :element
+           },
+           5 => %{
+             attrs: [],
+             children: [6],
+             id: 5,
+             name: "h1",
+             parent: 4,
+             type: :element
+           },
+           6 => %{contents: "Hello world", id: 6, parent: 5, type: :text}
+         },
+         root: 0
+       }}
+
+  """
   def flat_parse(html) when byte_size(html) > 500 do
     flat_parse_async(html)
   end
+
   def flat_parse(html) do
     flat_parse_sync(html)
   end
@@ -21,6 +89,7 @@ defmodule Html5ever do
     case Html5ever.Native.parse_sync(html) do
       {:html5ever_nif_result, :ok, result} ->
         {:ok, result}
+
       {:html5ever_nif_result, :error, err} ->
         {:error, err}
     end
@@ -28,9 +97,11 @@ defmodule Html5ever do
 
   defp parse_async(html) do
     :ok = Html5ever.Native.parse_async(html)
+
     receive do
       {:html5ever_nif_result, :ok, result} ->
         {:ok, result}
+
       {:html5ever_nif_result, :error, err} ->
         {:error, err}
     end
@@ -40,6 +111,7 @@ defmodule Html5ever do
     case Html5ever.Native.flat_parse_sync(html) do
       {:html5ever_nif_result, :ok, result} ->
         {:ok, result}
+
       {:html5ever_nif_result, :error, err} ->
         {:error, err}
     end
@@ -47,12 +119,13 @@ defmodule Html5ever do
 
   defp flat_parse_async(html) do
     :ok = Html5ever.Native.flat_parse_async(html)
+
     receive do
       {:html5ever_nif_result, :ok, result} ->
         {:ok, result}
+
       {:html5ever_nif_result, :error, err} ->
         {:error, err}
     end
   end
-
 end
--- lib/html5ever/native.ex
+++ lib/html5ever/native.ex
@@ -1,8 +1,8 @@
-defmodule NifNotLoadedError do
-  defexception message: "nif not loaded"
-end
-
 defmodule Html5ever.Native do
+  @moduledoc false
+
+  # This module will be replaced by the NIF module after
+  # loaded. It throws an error in case the NIF can't be loaded.
   use Rustler, otp_app: :html5ever, crate: "html5ever_nif", mode: :release
 
   def parse_sync(_binary), do: err()
@@ -10,7 +10,5 @@ defmodule Html5ever.Native do
   def flat_parse_sync(_binary), do: err()
   def flat_parse_async(_binary), do: err()
 
-  defp err() do
-    throw(NifNotLoadedError)
-  end
+  defp err, do: :erlang.nif_error(:nif_not_loaded)
 end
