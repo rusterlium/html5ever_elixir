8652de6727d879c3d653964bb0b84ce942d8c2ad
Philip Sampaio <philip.sampaio@gmail.com>
2023-01-30 12:42:48 -0300

    Support both 0.5 and 0.6 series of RustlerPrecompiled

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Html5ever.Mixfile do
 
   defp deps do
     [
-      {:rustler_precompiled, "~> 0.6.0"},
+      {:rustler_precompiled, "~> 0.5.2 or ~> 0.6.0"},
       {:rustler, ">= 0.0.0", optional: true},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
