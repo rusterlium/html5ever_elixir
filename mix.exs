12e2be1fcc9a20309a6b6fd662e6b9c86769a3fd
Philip Sampaio <philip.sampaio@gmail.com>
2023-06-16 16:39:29 -0300

    Drop support for Elixir 1.11 (#92)

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Html5ever.Mixfile do
     [
       app: :html5ever,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
