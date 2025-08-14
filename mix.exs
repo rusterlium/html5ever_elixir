8d4ae65c323ac0b33d7e5c3bd8ee8cf9a564150d
Philip Sampaio <philip.sampaio@gmail.com>
2025-08-14 11:49:50 -0300

    Remove support for Elixir 1.13 (#206)

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Html5ever.Mixfile do
     [
       app: :html5ever,
       version: @version,
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
