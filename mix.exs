003f7be711f9b2e28ec854040605e17205be3581
Philip Sampaio <philip.sampaio@gmail.com>
2026-03-31 12:48:02 -0300

    Require Elixir v1.15 or newer

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Html5ever.Mixfile do
     [
       app: :html5ever,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
